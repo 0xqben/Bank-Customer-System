546fffc44c7258e118134abf52d08bb1e45136d5
0xqben <nawar20062022.wow@gmail.com>
2026-07-28 05:17:25 +0300

    feat : test and add deposit to main menu

--- Bank-Customer-System.cpp
+++ Bank-Customer-System.cpp
@@ -20,7 +20,8 @@ enum enMainMenuOptions
     DeleteClient = 3,
     UpdateClient = 4,
     FindClient = 5,
-    ExitProgram = 6,
+    TranscationClients = 6,
+    ExitProgram = 7,
 };
 
 enum enTransactionMenuOptions
@@ -398,9 +399,9 @@ enMainMenuOptions ChooseOperation() {
     int Choose;
     do
     {
-        cout << "Choose what do you want to do ? [1 to 6]?" << endl;
+        cout << "Choose what do you want to do ? [1 to 7]?" << endl;
         cin >> Choose;
-    } while (Choose > 6 || Choose < 1);
+    } while (Choose > 7 || Choose < 1);
 
     return (enMainMenuOptions)Choose;
 }
@@ -613,6 +614,12 @@ void PerformMainMenuOption(enMainMenuOptions SelectedOption) {
         system("cls");
         UpdateClientScreen();
         GoBackToMainMenu();
+        break;
+
+    case enMainMenuOptions::TranscationClients :
+        system("cls");
+        ShowTransactionsScreen();
+        GoBackToMainMenu();
 
         break;
     case enMainMenuOptions::FindClient :
@@ -623,7 +630,7 @@ void PerformMainMenuOption(enMainMenuOptions SelectedOption) {
     case enMainMenuOptions::ExitProgram :
         system("cls");
         ShowEndScreen();
-
+        break;
     }
 
 }
@@ -638,7 +645,8 @@ void ShowMainMenu() {
     cout << "\t[3] Delete Client." << endl;
     cout << "\t[4] Update Client." << endl;
     cout << "\t[5] Find Client." << endl;
-    cout << "\t[6] Exit." << endl;
+    cout << "\t[6] transaction Client." << endl;
+    cout << "\t[7] Exit." << endl;
     cout << "======================================================\n";
     PerformMainMenuOption(ChooseOperation());
 }

d15a43dbffa39703093f6b135ff0e4ea7af46c5c
0xqben <nawar20062022.wow@gmail.com>
2026-07-28 05:27:27 +0300

    feat : add total balances functions

--- Bank-Customer-System.cpp
+++ Bank-Customer-System.cpp
@@ -498,6 +498,8 @@ void DepositClient(string AccountNumber, vector<stClient> &vClients) {
 
             }
         }
+
+        // Save new balance
         SaveClientsDataToFile(FileName, vClients);
         vClients = LoadClientsDataFromFile(FileName);
         
@@ -527,9 +529,41 @@ void ShowDepositWithdrawClientScreen() {
     //WithdrawClient();
 
 }
+
+void PrintClientRecordBalanceLine(stClient Client)
+{
+    cout << "| " << setw(15) << left << Client.AccountNumber;
+    cout << "| " << setw(40) << left << Client.Name;
+    cout << "| " << setw(12) << left << Client.Balance;
+}
+
 void TotalBanacess() {
 
-    TotalBanacess();
+    vector <stClient> vClients = LoadClientsDataFromFile(FileName);
+    cout << "\n\t\t\t\t\tBalances List (" << vClients.size() << ") Client(s).";
+        cout <<
+        "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+    cout << "| " << left << setw(15) << "Accout Number";
+    cout << "| " << left << setw(40) << "Client Name";
+    cout << "| " << left << setw(12) << "Balance";
+    cout <<
+        "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+    double TotalBalances = 0;
+
+    if (vClients.size() == 0)
+    {
+        cout << "\n\t NoClients available in the system\n";
+    }
+    else
+    {
+        for (stClient& C : vClients)
+        {
+            PrintClientRecordBalanceLine(Client);
+            TotalBalances += C.Balance;
+        }
+    }
 }
 
 void PerformTransactionOption(enTransactionMenuOptions SelectedOption) {
